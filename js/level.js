aa2a7dc995055c35745077455861fab83813bc5c
Steinar Myhre <s@2st.no>
2013-11-20 17:20:57 +0100

    Added levels.js

---
+++ js/level.js
@@ -0,0 +1,18 @@
+var levels = {
+	1: 'zxc',
+	2: 'asd',
+	3: 'qwe',
+	4: 'qaz',
+	5: 'wsx',
+	6: 'edc',
+	7: 'qsc',
+	8: 'zse',
+	9: 'qwedc',
+	10: 'qweasdzxc',
+	11: 'rtyu',
+	12: 'fghj',
+	13: 'vbnm',
+	14: 'iop√',
+	15: 'kl√√',
+	16: 'm,.-'	
+}
